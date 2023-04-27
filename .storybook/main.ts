79801356090f4b553bbc612781c7ed8a19e3b716
yasuhiro-y <yasuhiro.yokota@my-best.com>
2023-04-27 22:11:43 +0900

    initial commit

---
+++ .storybook/main.ts
@@ -0,0 +1,10 @@
+module.exports = {
+    stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+    addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  };
+module.exports = {
+  addons: ['@chakra-ui/storybook-addon'],
+  features: {
+    emotionAlias: false,
+  },
+}
